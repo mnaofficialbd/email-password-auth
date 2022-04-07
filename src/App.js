f4a7271cb4a7650e42c5b3dbc8c99618a2d5cfe0
mnaofficial <mna.official92@gmail.com>
2022-04-07 12:54:39 +0600

    bootstrap add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { getAuth } from "firebase/auth";
 import app from './firebase.init';
+import { Form } from 'react-bootstrap';
+import { Button } from 'bootstrap';
 
 const auth = getAuth(app);
 
 function App() {
 
-  const handleEmailBlur = (event) => {
+  /* const handleEmailBlur = (event) => {
     console.log(event.target.value);
   }
 
@@ -14,20 +17,45 @@ function App() {
     console.log(event.target.value);
   }
 
-  const handleSubmit=(event)=>{
+  const handleSubmit = (event) => {
     console.log('form submitted')
     event.preventDefault()
-  }
+  } */
   return (
     <div className="App">
       <h2>Email Password Authentication</h2>
-      <form onSubmit={handleSubmit}>
+
+      {/* <form onSubmit={handleSubmit}>
         <input onBlur={handleEmailBlur} type="email" placeholder='email' />
         <br />
         <input onBlur={handlePasswordBlur} type="password" name="" id="" placeholder='password' />
       <br />
       <input type="submit" value="Login" />
-      </form>
+      </form> */}
+
+      <div className='registration w-50 mx-auto'>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+
     </div>
   );
 }
